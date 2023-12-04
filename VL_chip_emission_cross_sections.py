f89852b2507a53627338cf52361ae5bed2d6bd6c
Limited User <limited.user@vitrealab.com>
2023-12-04 17:29:32 +0100

    added stage control

--- VL_chip_emission_cross_sections.py
+++ VL_chip_emission_cross_sections.py
@@ -19,6 +19,7 @@ if api_folder not in sys.path:
     site.addsitedir(str(api_folder))
 #from camera_control.tis_camera import TISCamera
 from camera_control.tis_camera_cmos import TISCamera
+import Kinesis.KDC101_Translation_Class as KDC101_Class 
 
 # reads mac address to identify PC
 mac = gma()
@@ -29,16 +30,19 @@ if mac == '18:c0:4d:26:9c:53':
     print("Using right PC")
     # define SN
     SN_LTS = 45158784  # stage LTS300 - long axis (X)
-    # SN_KDC = 27004871  # stage KCube DC Motor
+    SN_KDC = 27004871  # stage KCube DC Motor
     SN_PM = 'USB0::0x1313::0x8072::1907998::INSTR'  # bus PM
     SN_CAMERA = '16910441'
     SN_CAMERA = "41220696" #DMM 37UX265-ML
 
-#%% connect to camera
 
+#Connect stage
+z_stage = KDC101_Class.KDC101_translation(SN_KDC)
+
+
+# Connect camera
 cam1 = None
 del cam1
-
 cam1 = TISCamera(SN_CAMERA) #monochrome, SN 28020401
 
 cam1.set_format('Y16 (1280x1024)')
@@ -46,76 +50,109 @@ cam1.set_format('Y16 (640x480)')
 cam1.set_format('Y16 (2048x1536)')
 print(cam1.get_format())
 
-cam1.set_gain(21.6)
-cam1.set_exposure(1/21)
 
-
+#%%connect to z-stage
+z_stage = KDC101_Class.KDC101_translation(SN_KDC)
+out_z_coord = z_stage.read_position()
+print(out_z_coord)
 
 
 #%%
-camera = cam1
+delta_z = -0.1
+z_stage.rel_move(delta_z)
+out_z_coord = z_stage.read_position()
+print(out_z_coord)
 
-inlineOrWindow = 0 # inline=0 window=1
-
-exp_time = 1/50
-camera_gain = 0
-FPS = 30
-
-camera.set_framerate(FPS)
-camera.set_exposure(exp_time) #seconds
-camera.set_gain(camera_gain)
-
-print('FPS: %.4f' %(camera.get_framerate()))
-print('Exposure: %.10f (s)' %(camera.get_exposure()))
-print('Gain: %.4f (dB)' %(camera.get_gain()))
-
-
-img = camera.snap_image() #save image
-
-
-if not inlineOrWindow:
-    get_ipython().run_line_magic('matplotlib', 'inline')
-else:
-    get_ipython().run_line_magic('matplotlib', 'qt5')
-
-
-if len(np.shape(img))==2: # monochrome
-   
-    plt.clf()
-    # plt.imshow(img, cmap='coolwarm')
-    plt.imshow(img, cmap='plasma')
-    plt.title('SN:' + camera.sn + ', Format:' + camera.get_format())
-    plt.colorbar()
-    plt.show()
+#%%
+def abs_move_stage(abs_pos):
     
-    print(' ')
-    print('Mean:' + str(np.mean(img)))
-    print('Max: ' + str(np.max(img)))
+    if 0<abs_pos<24.5:
+        out_z_coord_start = z_stage.read_position()
+        z_stage.abs_move(float(abs_pos))
+        out_z_coord_stop = z_stage.read_position()
+        print(f"Start position: {out_z_coord_start:.2f}mm")
+        print(f"Stop position: {out_z_coord_stop:.2f}mm")
+    else:
+        print("Give a position in the range [0-25mm]")
     
+    
+def rel_move_stage(rel_step = +0.1):
+    rel_step = float(rel_step)
+    
+    if 0.001<rel_step<1.0:
+        z_stage = KDC101_Class.KDC101_translation(SN_KDC)
+        out_z_coord_start = z_stage.read_position()
+        z_stage.rel_move(float(rel_step))
+        out_z_coord_stop = z_stage.read_position()
+        print(f"Start position: {out_z_coord_start:.2f}mm")
+        print(f"Stop position: {out_z_coord_stop:.2f}mm")
+    else:
+        print("Give a valid relative jog size: [0.001-1.0]mm")
+
+
+def snap_image(exp_time = 1/5000, camera_gain = 0, FPS = 5, plot=True):
+    
+    inlineOrWindow = 0 # inline=0 window=1
+    
+    #exp_time = 1/5000
+    #camera_gain = 0
+    #FPS = 5
+    
+    cam1.set_framerate(FPS)
+    cam1.set_exposure(exp_time) #seconds
+    cam1.set_gain(camera_gain)
+    
+    print('FPS: %.4f' %(cam1.get_framerate()))
+    print('Exposure: %.10f (s)' %(cam1.get_exposure()))
+    print('Gain: %.4f (dB)' %(cam1.get_gain()))
+    
+    
+    img = cam1.snap_image() #save image
+    
+    if plot:
+        if not inlineOrWindow:
+            get_ipython().run_line_magic('matplotlib', 'inline')
+        else:
+            get_ipython().run_line_magic('matplotlib', 'qt5')
         
-elif len(np.shape(img))==3: # RGB
-      
-    img_norm = img[:,:,[0,1,2,3]]/(2**16) #normalize so that we can plot an RGB image
-    # img_norm = np.uint8(img) #normalize
-    img_norm_flip = np.flipud(img_norm)
-       
-    img_show = img_norm_flip
-    img_show = img_norm
+        
+        if len(np.shape(img))==2: # monochrome
+           
+            plt.clf()
+            # plt.imshow(img, cmap='coolwarm')
+            plt.imshow(img, cmap='plasma')
+            plt.title('SN:' + cam1.sn + ', Format:' + cam1.get_format())
+            plt.colorbar()
+            plt.show()
+            
+            print(' ')
+            print('Mean:' + str(np.mean(img)))
+            print('Max: ' + str(np.max(img)))
+            
+                
+        elif len(np.shape(img))==3: # RGB
+              
+            img_norm = img[:,:,[0,1,2,3]]/(2**16) #normalize so that we can plot an RGB image
+            # img_norm = np.uint8(img) #normalize
+            img_norm_flip = np.flipud(img_norm)
+               
+            img_show = img_norm_flip
+            img_show = img_norm
+        
+            
+            plt.clf()
+            # plt.imshow(img_show, cmap='coolwarm')
+            plt.imshow(img_show, cmap='plasma')
+            plt.title('SN:' + cam1.sn + ', Format:' + cam1.get_format())
+            plt.show()
+            
+            print(' ')
+            print('Mean (16 bit): R=%.2f, G=%.2f, B=%.2f' %(np.mean(img[:,:,0]),np.mean(img[:,:,1]),np.mean(img[:,:,2])))
+            print('Max (16 bit=65536): R=%.2f, G=%.2f, B=%.2f' %(np.max(img[:,:,0]),np.max(img[:,:,1]),np.max(img[:,:,2])))
 
-    
-    plt.clf()
-    # plt.imshow(img_show, cmap='coolwarm')
-    plt.imshow(img_show, cmap='plasma')
-    plt.title('SN:' + camera.sn + ', Format:' + camera.get_format())
-    plt.show()
-    
-    print(' ')
-    print('Mean (16 bit): R=%.2f, G=%.2f, B=%.2f' %(np.mean(img[:,:,0]),np.mean(img[:,:,1]),np.mean(img[:,:,2])))
-    print('Max (16 bit=65536): R=%.2f, G=%.2f, B=%.2f' %(np.max(img[:,:,0]),np.max(img[:,:,1]),np.max(img[:,:,2])))
-#%%
-       
+ #%%      
 if not True: # save image
-#%%     
+     
     now = datetime.datetime.now() #date and time
     today = now.strftime("%Y-%m-%d") #date
     now_time = now.strftime("%H.%M.%S") #time
